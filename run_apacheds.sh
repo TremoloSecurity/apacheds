a178ca368169dede4af8ef8c478885f5ad0431ef
Johannes Kastl <kastl@b1-systems.de>
2023-11-02 11:29:53 +0100

    run_apacheds.sh: use $(...) notation for CTX_ENTRY

--- run_apacheds.sh
+++ run_apacheds.sh
@@ -130,12 +130,12 @@ export RDN=${DN_COMP/=/: }
 RDN_VAL="$(echo "${DN_COMP}" | sed 's/.*=//')"
 export RDN_VAL
 
-CTX_ENTRY=`base64 -w 0 <<EOF
+CTX_ENTRY=$(base64 -w 0 <<EOF
 dn: $DN
 objectClass: $OBJECT_CLASS
 $RDN
-EOF`
-
+EOF
+)
 export CTX_ENTRY
 
 ldapmodify -H ldap://127.0.0.1:10389 -D uid=admin,ou=system -w secret -a <<EOF
